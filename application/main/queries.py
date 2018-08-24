13318995da005d808caf6763b77511dd5b8a5503
Charity Marani <marani.charity95@gmail.com>
2018-08-24 09:12:54 +0300

    Update queries.py

--- application/main/queries.py
+++ application/main/queries.py
@@ -1,29 +1,29 @@
 '''
 Querying the database
 '''
-query1 = """
-    INSERT INTO questions(topic,title, description, owner, answers)
+query4 = """
+    INSERT INTO questions(topic,title, details, owner, answers)
     VALUES(%s, %s, %s, %s, %s) RETURNING id
     """
 
-query2 = """
+query1 = """
     CREATE TABLE IF NOT EXISTS users(
     id serial PRIMARY KEY, name VARCHAR,
     username VARCHAR, email VARCHAR,
-    password VARCHAR, confirm_password VARCHAR
+    password VARCHAR
     )
     """
-query3 = """
+query2 = """
     CREATE TABLE IF NOT EXISTS questions(
     id serial PRIMARY KEY,topic VARCHAR,
-    title VARCHAR, description VARCHAR,
+    title VARCHAR, details VARCHAR,
     owner INTEGER,
     answers INTEGER,
     FOREIGN KEY (owner) REFERENCES users(id) ON DELETE CASCADE
     )
     """
 
-query4 = """
+query3 = """
     CREATE TABLE IF NOT EXISTS answers(
     id serial PRIMARY KEY,
     answer VARCHAR, accepted BOOL,
